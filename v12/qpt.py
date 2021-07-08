3184d34685994b6944b63bbec30771d1de22356a
Aritra Sarkar <asarkar5@tudelft.nl>
2021-07-08 15:09:27 +0200

    QPT 1 qubit working

--- v12/qpt.py
+++ v12/qpt.py
@@ -17,6 +17,16 @@ hsdim = 2**qubits           # Hilbert space dimension
 trials = 1024               # Number of tomographic trials for each observable
 aprx = 2                    # Places of decimal to round the reconstructed density matrix
 
+
+'''
+The "black-box" quantum process is defined here
+'''
+def QProcess(qcirc, qreg):
+    qcirc.barrier()
+    qcirc.h(qreg[0])
+    qcirc.barrier()
+    return
+
 '''
 Make entangled state
 '''
@@ -28,32 +38,17 @@ def EntangleAncilla(qcirc):
     return
 
 '''
-The "black-box" quantum process is defined here
+Add post-rotation gates and eigenvalue sign for Quantum State Tomography
 '''
-def QProcess(qcirc):
-    qcirc.barrier()
-    qcirc.h(0)
-    qcirc.barrier()
-    return
-
-qcirc = QuantumCircuit(qubits, qubits)
-EntangleAncilla(qcirc)
-QProcess(qcirc)
-print(qcirc)
-
-rho = qi.DensityMatrix.from_instruction(qcirc)
-print("\nActual Density Matrix")
-print(rho.data)
-
 def PostRot(ps,qcirc):
-    P0 = [[1,0],[0,1]]
-    P1 = [[0,1],[1,0]]
-    P2 = [[0,complex(0,-1)],[complex(0,1),0]]
-    P3 = [[1,0],[0,-1]]
-    E0 = [1, 1]
-    E1 = [1, -1]
-    E2 = [1, -1]
-    E3 = [1, -1]
+    P0 = [[1,0],[0,1]]                          # I
+    P1 = [[0,1],[1,0]]                          # Pauli-X
+    P2 = [[0,complex(0,-1)],[complex(0,1),0]]   # Pauli-Y
+    P3 = [[1,0],[0,-1]]                         # Pauli-Z
+    E0 = [1, 1]                                 # Eigenvalues of P0
+    E1 = [1, -1]                                # Eigenvalues of P1
+    E2 = [1, -1]                                # Eigenvalues of P2
+    E3 = [1, -1]                                # Eigenvalues of P3
     B = 1
     E = 1
     q = 0
@@ -74,9 +69,12 @@ def PostRot(ps,qcirc):
             E = kron(E3,E)
         q += 1
     qcirc.barrier()
-    qcirc.measure([0,1], [0,1]) # make this scalable
+    qcirc.measure([0,1], [0,1])                 # TBD: make this scalable
     return(B,E,qcirc)
 
+'''
+Convert a decimal number to base-n
+'''
 def toStr(n,base):
    convertString = "0123456789ABCDEF"
    if n < base:
@@ -91,6 +89,9 @@ def dict2hist(counts):
             phist[i] = counts.get(str(bin(i)[2:]).zfill(qubits)) / trials
     return phist
 
+'''
+Quantum State Tomography
+'''
 def QST():
     # fname = open("AAQPT_full.txt", "w")
     dm = np.zeros((hsdim, hsdim)) * 0j
@@ -107,13 +108,51 @@ def QST():
     # fname.close()
     return est_rho
 
+def partialTrace1(dm):
+    dm1 = np.zeros((2,2)) * 0j
+    for i in range(0,2):
+        for j in range(0,2):
+            dm1[i][j] = dm[i][j]+dm[i+2][j+2]
+    return dm1
+
+qcirc = QuantumCircuit(qubits, qubits)
+EntangleAncilla(qcirc)
+QProcess(qcirc, [0])
+print(qcirc)
+
+rho = qi.DensityMatrix.from_instruction(qcirc)                                                                                                  # TEST QST
+print("\nActual Density Matrix")                                                                                                                # TEST QST
+print(rho.data)                                                                                                                                 # TEST QST
+
 print("\nEstimated Density Matrix")
 rho_choi = np.round(QST(),aprx)
 print(rho_choi)
 
-"""
+psi_inp = QuantumCircuit(process_qubits, process_qubits)
+psi_inp.i(0)
+rho_inp = qi.DensityMatrix.from_instruction(psi_inp).data
+print("\nInput Density Matrix")
+print(rho_inp)
 
-(qeait) D:\GoogleDrive\RESEARCH\A1 - Programs\AutonomousQuantumPhysicist\v12>python qpt.py
+# Use the quantum process to evolve the density matrix
+QProcess(psi_inp, [0])                                                                                                                          # TEST QPT
+rho_out = qi.DensityMatrix.from_instruction(psi_inp).data                                                                                       # TEST QPT 
+print("\nActual Output Density Matrix")                                                                                                         # TEST QPT 
+print(rho_out)                                                                                                                                  # TEST QPT
+
+rho_out_choi = 2**process_qubits * partialTrace1( np.matmul( np.kron(np.transpose(rho_inp), np.eye(2**process_qubits) ), rho.data ))            # TEST QPT      
+print("\nOutput Density Matrix using Actual Choi Matrix")                                                                                       # TEST QPT
+print(rho_out_choi)                                                                                                                             # TEST QPT      
+
+# Use the estimated Choi matrix to predict the output density matrix 
+rho_out_choi_est = 2**process_qubits * partialTrace1( np.matmul( np.kron(np.transpose(rho_inp), np.eye(2**process_qubits) ), rho_choi ))
+print("\nOutput Density Matrix using Estimated Choi Matrix")
+print(rho_out_choi_est)
+
+'''
+
+(qeait) D:\GoogleDrive\RESEARCH\A1 - Programs\QKSA\v12>python qpt.py
+
       ░      ┌───┐ ░  ░ ┌───┐ ░
 q_0: ─░──────┤ X ├─░──░─┤ H ├─░─
       ░ ┌───┐└─┬─┘ ░  ░ └───┘ ░
@@ -129,63 +168,25 @@ Actual Density Matrix
  [-0.25+0.j -0.25+0.j -0.25+0.j  0.25+0.j]]
 
 Estimated Density Matrix
-[[ 0.26+0.j    0.25+0.02j  0.25+0.02j -0.25-0.j  ]
- [ 0.25-0.02j  0.25+0.j    0.25+0.01j -0.25+0.j  ]
- [ 0.25-0.02j  0.25-0.01j  0.24+0.j   -0.25+0.01j]
- [-0.25+0.j   -0.25-0.j   -0.25-0.01j  0.25+0.j  ]]
+[[ 0.25+0.j    0.25-0.01j  0.25+0.01j -0.25+0.02j]
+ [ 0.25+0.01j  0.24+0.j    0.25-0.j   -0.25+0.01j]
+ [ 0.25-0.01j  0.25+0.j    0.26+0.j   -0.25+0.01j]
+ [-0.25-0.02j -0.25-0.01j -0.25-0.01j  0.24+0.j  ]]
 
-"""
+Input Density Matrix
+[[1.+0.j 0.+0.j]
+ [0.+0.j 0.+0.j]]
 
-def partialTrace1(dm):
-    dm1 = np.zeros((2,2)) * 0j
-    for i in range(0,2):
-        for j in range(0,2):
-            dm1[i][j] = dm[i][j]+dm[i+2][j+2]
-    return dm1
+Actual Output Density Matrix
+[[0.5+0.j 0.5+0.j]
+ [0.5+0.j 0.5+0.j]]
 
-psi_inp = QuantumCircuit(process_qubits, process_qubits)
-psi_inp.i(0)
-rho_inp = qi.DensityMatrix.from_instruction(psi_inp).data
+Output Density Matrix using Actual Choi Matrix
+[[0.5+0.j 0.5+0.j]
+ [0.5+0.j 0.5+0.j]]
 
-print("\nInput State Preparation Circuit")
-print(psi_inp)
-print("\nInput Density Matrix")
-print(rho_inp)
+Output Density Matrix using Estimated Choi Matrix
+[[0.5 +0.j   0.5 -0.02j]
+ [0.5 +0.02j 0.48+0.j  ]]
 
-rho_anc = rho_inp # np.eye(2**process_qubits)
-
-# Use the quantum process to evolve the density matrix
-QProcess(psi_inp)
-rho_out = qi.DensityMatrix.from_instruction(psi_inp).data
-print("\nOutput Density Matrix")
-print(rho_out)
-
-print("\nChoi Matrix")
-print(qi.Choi(psi_inp).data / 2**process_qubits)
-
-# Evolution of a DensityMatrix ρ with respect to the Choi-matrix is given by: ρ-new = Tr1 [ Λ (ρT ⊗ I) ]
-
-# Use the original Choi matrix to predict the output density matrix 
-rho_sys = np.kron(np.transpose(rho_inp),rho_anc) 
-print(rho_sys)
-
-print("\nrho_sys_out options:")
-# rho_sys_out = np.matmul(rho.data,np.matmul(rho_sys,np.transpose(rho.data))) # NOT WORKING HERE... trace not 1
-# print(rho_sys_out)    
-rho_sys = np.kron(np.transpose(rho_inp),rho_anc) 
-rho_sys_out = np.matmul(rho.data,rho_sys)   # NOT WORKING HERE... trace not 1
-print(rho_sys_out)    
-rho_sys_out = np.matmul(rho_sys,rho.data)   # NOT WORKING HERE... trace not 1
-print(rho_sys_out)           
-rho_sys = np.kron(rho_anc,np.transpose(rho_inp)) 
-rho_sys_out = np.matmul(rho.data,rho_sys)   # NOT WORKING HERE... trace not 1
-print(rho_sys_out)    
-rho_sys_out = np.matmul(rho_sys,rho.data)   # NOT WORKING HERE... trace not 1
-print(rho_sys_out)           
-
-# rho_out_choi = partialTrace1(rho_sys_out)
-# print(rho_out_choi)
-
-# # Use the estimated Choi matrix to predict the output density matrix 
-# rho_est = 
-# print(rho_est)
+ '''
