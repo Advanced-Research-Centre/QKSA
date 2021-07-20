d25352043f542d7cd3562a3e62125a95be765aa7
Aritra <aritra44sarkar@yahoo.co.in>
2021-07-20 13:32:47 +0200

    test on random 1-qubit

--- v13/environment.py
+++ v13/environment.py
@@ -1,5 +1,5 @@
 from qiskit import QuantumCircuit, Aer, execute
-from random import randint
+from random import randint, uniform
 from math import pi
 from copy import deepcopy
 
@@ -21,7 +21,7 @@ class environment:
 		print("\n. . . environment being setup . . .\n")	
 		if qcirc is None:
 			qcirc = QuantumCircuit(self.num_qb)
-			stateSel = int(input("1: All-Zeros\n2: Equal Superposition\n3: Random Pauli\n4: GHZ-state\n5: W-state\n\nChoose Environment State : ") or "2")
+			stateSel = int(input("1: All-Zeros\n2: Equal Superposition\n3: Random Pauli\n4: Random U\n5: GHZ-state\n6: W-state\n\nChoose Environment State : ") or "2")
 			if stateSel == 1:
 				print("All-Zero state on "+str(self.num_qb)+" qubits selected")
 			elif stateSel == 2:
@@ -37,13 +37,20 @@ class environment:
 					elif axis == 1:
 						qcirc.rx(pi/2,i)
 					elif axis == 2:
-						qcirc.ry(pi/2,i)		
+						qcirc.ry(pi/2,i)	
 			elif stateSel == 4:
+				print("Random U-gate state on "+str(self.num_qb)+" qubits selected")
+				for i in range(0,self.num_qb):
+					a_theta = uniform(0, pi)
+					a_phi = uniform(0, pi)
+					a_lambda = uniform(0, pi)
+					qcirc.u(a_theta, a_phi, a_lambda, i)	
+			elif stateSel == 5:
 				print("GHZ-state on "+str(self.num_qb)+" qubits selected")
 				qcirc.h(0)
 				for i in range(1,self.num_qb):
 					qcirc.cx(0,i)
-			elif stateSel == 5:
+			elif stateSel == 6:
 				print("W-state on "+str(self.num_qb)+" qubits selected")
 				# https://quantumcomputing.stackexchange.com/questions/4350/general-construction-of-w-n-state
 				print("current bug = future feature!")
--- v13/sandbox/bao/task4.py
+++ v13/sandbox/bao/task4.py
@@ -60,3 +60,24 @@ if '0' in counts.keys():
         print("f1 or f2")
 else:
     print("f3")
+
+'''
+Fn#     Name of the Boolean Function	        Boolean Function        Meaning
+----------------------------------------------------------------------------------------------------------
+f00     Null	                                0                       Always 0
+f01     Identity	                            1                       Always 1
+f02     Transfer	                            A                       Pass value of A
+f03                                             B                       Pass value of B
+f04     NOT	                                    !A                      Pass negated value of A
+f05                                             !B                      Pass negated value of B
+f06     AND	                                    A∙B                     1 only if A and B both are 1
+f07     NAND	                                !(A∙B)                  0 only if A and B both are 1
+f08     OR	                                    A+B                     0 only if A and B both are 0
+f09     NOR	                                    !(A+B)                  1 only if A and B both are 0
+f10     Implication	                            A+(!B)                  If B, then A
+f11                                             (!A)+B                  If A, then B
+f12     Inhibition	                            A∙(!B)                  A but not B
+f13                                             !(A)∙B                  B but not A
+f14     EX-OR	                                A⊕B                     or B, but not both
+f15     EX-NOR	                                !(A⊕B)                  if A equals B
+'''
